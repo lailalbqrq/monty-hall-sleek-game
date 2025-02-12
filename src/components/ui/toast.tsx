f81ff6a0be5e5a595a73a05465e06f31afbc30e9
Laila <155716815+lailalbqrq@users.noreply.github.com>
2025-02-12 19:16:40 +0800

    Update toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,12 +14,13 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed inset-0 z-[100] flex items-center justify-center p-4", // Center the toast
+      "fixed top-0 left-0 right-0 bottom-0 z-[100] flex items-center justify-center p-4 pointer-events-none", // Center the toast
       className
     )}
     {...props}
   />
-))
+));
+ToastViewport.displayName = ToastPrimitives.Viewport.displayName;
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName
 
 const toastVariants = cva(
